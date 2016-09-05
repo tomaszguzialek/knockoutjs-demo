fa62f1e27e7cf7967346e20cc62f667048b2940d
Tomasz Guzialek <tomasz@guzialek.info>
2016-09-05 21:53:07 +0200

    Using datatables.net to show feature_requests.

--- src/components/feature_requests/feature_requests.js
+++ src/components/feature_requests/feature_requests.js
@@ -1,16 +1,24 @@
 import ko from 'knockout';
+import 'datatables';
 import featureRequestsTemplate from 'text!./feature_requests.html';
 
 class FeatureRequestsViewModel {
     constructor(route) {
-        this.feature_requests = ko.observableArray([]);
         this.getAllFeatureRequests();
     }
 
     getAllFeatureRequests() {
         var self = this;
         $.getJSON('http://localhost:5000/v1/feature_request', function (data) {
-          self.feature_requests(data.feature_requests);
+          $('#feature_requests_dt').DataTable({
+            data: data.feature_requests,
+            columns: [
+              { data: 'id' },
+              { data: 'client_id' },
+              { data: 'title' },
+              { data: 'description' }
+            ]
+          });
         })
     }
 }
