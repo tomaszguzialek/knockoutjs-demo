a17ef6ac00088f2d5b9079bce301408f71899ed7
Tomasz Guzialek <tomasz@guzialek.info>
2016-09-20 22:33:47 +0200

    Login form setting cookie.

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -6,6 +6,32 @@ import { app_config } from 'app/app_config';
 
 class LoginViewModel {
     constructor(route) {
+      this.login = ko.observable();
+      this.password = ko.observable();
+    }
+
+    signIn() {
+      var self = this;
+
+      var data = {
+        login: self.login(),
+        password: self.password()
+      };
+
+      $.ajax({
+        url: 'http://' + app_config.api_host + '/v1/auth/login',
+        data: ko.toJSON(data),
+        type: 'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: "text",
+        success: function (response) {
+          var json = $.parseJSON(response);
+          Cookie.set('token', json.token);
+        },
+        error: function (error) {
+          console.error(error);
+        }
+      });
     }
 }
 
