ce8359d1d3cc4e9d46e73a5e1cdd3a8d1ee95005
Tomasz Guzialek <tomasz@guzialek.info>
2016-09-20 23:59:49 +0200

    Fixing top nav-bar links.

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,12 +1,17 @@
 import ko from 'knockout';
 import template from 'text!./nav-bar.html';
+import * as router from 'app/router';
 
 class NavBarViewModel {
     constructor(params) {
         // This viewmodel doesn't do anything except pass through the 'route' parameter to the view.
         // You could remove this viewmodel entirely, and define 'nav-bar' as a template-only component.
         // But in most apps, you'll want some viewmodel logic to determine what navigation options appear.
-        this.route = params.route;        
+        this.route = params.route;
+    }
+
+    linkTo(componentName) {
+      router.hasher.setHash(componentName);
     }
 }
 
