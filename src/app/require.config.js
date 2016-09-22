4effea8d8e03cd13fc8a97b1c746189acaf12574
Tomasz Guzialek <tomasz@guzialek.info>
2016-09-22 19:59:47 +0200

    Adding knockout-postbox.

--- src/app/require.config.js
+++ src/app/require.config.js
@@ -8,6 +8,7 @@ var require = {
         "jquery":               "bower_modules/jquery/dist/jquery",
         "knockout":             "bower_modules/knockout/dist/knockout",
         "knockout-projections": "bower_modules/knockout-projections/dist/knockout-projections",
+        "knockout-postbox":     "bower_modules/knockout-postbox/build/knockout-postbox.min",
         "signals":              "bower_modules/js-signals/dist/signals.min",
         "text":                 "bower_modules/requirejs-text/text",
         "datatables":           "bower_modules/datatables.net/js/jquery.dataTables.min",
